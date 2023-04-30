2e28a011146a532fe54d07d8b5e8eaa712f0dbb0
sungjun1116 <opr99@naver.com>
2023-04-30 21:25:33 +0900

    #83 - BoardPrincipal: OAuth 인증 정보를 다룰 수 있도록 기능 추가
    `OAuth2User` 인터페이스를 구현해서
    Spring OAuth Client 도 기존과 동일하게
    인증 정보를 인식할 수 있게끔 함

--- src/main/java/com/board/projectboard/dto/security/BoardPrincipal.java
+++ src/main/java/com/board/projectboard/dto/security/BoardPrincipal.java
@@ -6,8 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -17,9 +19,15 @@ public record BoardPrincipal(
         Collection<? extends GrantedAuthority> authorities,
         String email,
         String nickname,
-        String memo
-) implements UserDetails {
-    public static BoardPrincipal of(final String username, final String password, final String email, final String nickname, final String memo) {
+        String memo,
+        Map<String, Object> oAuth2Attributes
+) implements UserDetails, OAuth2User {
+
+    public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
+        return of(username, password, email, nickname, memo, Map.of());
+    }
+
+    public static BoardPrincipal of(final String username, final String password, final String email, final String nickname, final String memo, Map<String, Object> oAuth2Attributes) {
         // 지금은 인증만 하고 권한을 다루고 있지 않아서 임의로 세팅한다.
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
 
@@ -32,7 +40,8 @@ public record BoardPrincipal(
                         .collect(Collectors.toUnmodifiableSet()),
                 email,
                 nickname,
-                memo
+                memo,
+                oAuth2Attributes
         );
     }
 
@@ -89,6 +98,14 @@ public record BoardPrincipal(
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override public Map<String, Object> getAttributes() {
+        return oAuth2Attributes;
+    }
+
+    @Override public String getName() {
+        return username;
     }
 
     @RequiredArgsConstructor @Getter
